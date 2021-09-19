82bd6f80a76f0eb427ec20265252ca648fbb28b7
GuilhermoCampos <gsoldier95@yahoo.com>
2021-09-19 06:36:43 -0300

    init

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 
-namespace fibonacci
-{
-    class Program
-    {
+namespace fibonacci{
+    class Program{
         static void Main(string[] args){
             int termo;
             
@@ -16,7 +14,7 @@ namespace fibonacci
             TimeSpan fimNRec = new TimeSpan(tempoFimNRec.Ticks - tempoInicioNRec.Ticks);
             Console.WriteLine("Em {0} Milisegundos\n\n", fimNRec.Milliseconds);
 
-             DateTime tempoInicioRec = DateTime.Now;
+            DateTime tempoInicioRec = DateTime.Now;
             Console.WriteLine("A Função Recursiva de Fibonacci deu o resultado de {0}", fibonacciRecursivo(termo));
             DateTime tempoFimRec = DateTime.Now;
             TimeSpan fimRec = new TimeSpan(tempoFimRec.Ticks - tempoInicioRec.Ticks);
